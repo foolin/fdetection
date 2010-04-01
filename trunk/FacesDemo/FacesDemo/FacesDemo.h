999b21d8cfd9e7f5b9504e26ab04876e7f69f4b3
foolin55@gmail.com <foolin55@gmail.com@946ed153-6c77-64d3-6a7d-126bc18e93db>
2010-04-01 07:52:53 +0000

    重新上传

---
+++ trunk/FacesDemo/FacesDemo/FacesDemo.h
@@ -0,0 +1,42 @@
+// FacesDemo.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+#include "cv.h"
+#include "highgui.h"
+
+#include <stdarg.h>
+#include <string.h>
+#include <time.h>
+
+#define IMAGE_WIDTH 264
+#define IMAGE_HEIGHT 264
+#define IMAGE_CHANNELS 3
+
+
+// CFacesDemoApp:
+// 有关此类的实现，请参阅 FacesDemo.cpp
+//
+
+class CFacesDemoApp : public CWinApp
+{
+public:
+	CFacesDemoApp();
+
+// 重写
+	public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CFacesDemoApp theApp;
---
+++ trunk/FacesDemo/FacesDemo/FacesDemoDlg.h
@@ -0,0 +1,42 @@
+// FacesDemoDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CFacesDemoDlg 对话框
+class CFacesDemoDlg : public CDialog
+{
+// 构造
+public:
+	CFacesDemoDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_FACESDEMO_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOpenImage();
+	IplImage* m_readImage;
+
+public:
+	void ShowImage( IplImage* img, UINT ID );
+	void ResizeImage(IplImage* img);
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedSaveImage();
+	afx_msg void OnBnClickedAboutUs();
+	afx_msg void OnBnClickedDetectFace();
+};
