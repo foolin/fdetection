5d7bab88ca4078a32d2ec53ff9f0ae3b38a960bc
foolin55@gmail.com <foolin55@gmail.com@946ed153-6c77-64d3-6a7d-126bc18e93db>
2010-04-03 05:26:41 +0000

    增加缩小到托盘时，右键弹出菜单，增加二值化函数~

--- FacesDemo/FacesDemo/Resource.h
+++ FacesDemo/FacesDemo/Resource.h
@@ -7,6 +7,7 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_FACESDEMO_DIALOG            102
 #define IDR_MAINFRAME                   128
+#define IDR_MENU_TRAY                   131
 #define IDC_OPEN_IMAGE                  1000
 #define IDC_DETECT_FACE                 1001
 #define IDC_IMAGE                       1002
@@ -14,16 +15,25 @@
 #define IDC_REMOVE_NOISE                1004
 #define IDC_BINARY_IMAGE                1005
 #define IDC_ABOUT_US                    1006
-#define IDC_MINIMIZE1                   1009
-#define IDC_BUTTON1                     1010
 #define IDC_MINIMIZE                    1010
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define IDM_SHOW                        32775
+#define IDM_ABOUT                       32776
+#define IDM_QUIT                        32777
+#define ID_32778                        32778
+#define ID_                             32779
+#define IDM_TRAY_OPEN                   32780
+#define IDM_TRAY_ABOUT                  32781
+#define IDM_TRAY_QUIT                   32782
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32783
 #define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
