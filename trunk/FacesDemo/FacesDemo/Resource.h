b00f41ac8f0ec938213d6445034fe460580a8665
foolin55@gmail.com <foolin55@gmail.com@946ed153-6c77-64d3-6a7d-126bc18e93db>
2010-04-07 12:00:04 +0000

    1、增加顶部菜单 2、删除去噪声按钮 3、删除二值化按钮 4、修改部分菜单ID资源标识符

--- trunk/FacesDemo/FacesDemo/Resource.h
+++ trunk/FacesDemo/FacesDemo/Resource.h
@@ -8,15 +8,18 @@
 #define IDD_FACESDEMO_DIALOG            102
 #define IDR_MAINFRAME                   128
 #define IDR_MENU_TRAY                   131
+#define IDR_MENU_TOP                    132
 #define IDC_OPEN_IMAGE                  1000
 #define IDC_DETECT_FACE                 1001
 #define IDC_IMAGE                       1002
 #define IDC_SAVE_IMAGE                  1003
-#define IDC_REMOVE_NOISE                1004
-#define IDC_BINARY_IMAGE                1005
 #define IDC_ABOUT_US                    1006
 #define IDC_MINIMIZE                    1010
 #define IDC_STATIC_TIPS                 1011
+#define IDC_OPEN_FACES_DIR              1012
+#define IDC_RunCam                      1013
+#define IDC_StopCam                     1014
+#define IDC_CamList                     1017
 #define ID_32772                        32772
 #define ID_32773                        32773
 #define ID_32774                        32774
@@ -28,14 +31,58 @@
 #define IDM_TRAY_OPEN                   32780
 #define IDM_TRAY_ABOUT                  32781
 #define IDM_TRAY_QUIT                   32782
+#define ID_32783                        32783
+#define ID_32784                        32784
+#define ID_32785                        32785
+#define ID_32786                        32786
+#define ID_32787                        32787
+#define ID_32788                        32788
+#define ID_32789                        32789
+#define ID_32790                        32790
+#define IDM_OpenImage                   32791
+#define IDM_RunCamera                   32792
+#define IDM_Tray                        32793
+#define IDM_HomePage                    32794
+#define IDM_MiniTray                    32795
+#define IDM_Mini2Tray                   32796
+#define ID_SHOW                         32797
+#define IDM_Sh                          32798
+#define IDM_Show_                       32799
+#define IDM_Aout_                       32800
+#define IDM_Quit_                       32801
+#define ID_MOpenImage                   32802
+#define ID_MRunCamera                   32803
+#define ID_MMini2Tray                   32804
+#define ID_M2Tray                       32805
+#define ID_MQuit                        32806
+#define ID_Quit                         32807
+#define ID_Min2Tray                     32808
+#define ID_RunCamera                    32809
+#define ID_OpenImage                    32810
+#define ID_About                        32811
+#define ID_HomePage                     32812
+#define ID_Open                         32813
+#define ID_32814                        32814
+#define ID_StopCamera                   32815
+#define ID_32816                        32816
+#define ID_SaveImage                    32817
+#define ID_Menu                         32818
+#define ID_32819                        32819
+#define ID_FaceDect                     32820
+#define ID_OpenFaceDir                  32821
+#define ID_Config                       32822
+#define ID_OpenFacesDir                 32823
+#define ID_32824                        32824
+#define ID_GuestBook                    32825
+#define ID_PostMsg                      32826
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         32783
-#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32827
+#define _APS_NEXT_CONTROL_VALUE         1018
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
