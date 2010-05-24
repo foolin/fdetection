c21d6e4511ccf653c1a0e3e4e97b80f68fd44a01
foolin55@gmail.com <foolin55@gmail.com@946ed153-6c77-64d3-6a7d-126bc18e93db>
2010-05-24 03:00:24 +0000

    增加人脸检测注释

--- trunk/Face/Face/Detect.h
+++ trunk/Face/Face/Detect.h
@@ -19,14 +19,13 @@
 #define FACE_HEIGHT 128
 
 //读取图像
-#define IMAGE_READ_DEPTH IPL_DEPTH_8U
-#define IMAGE_READ_CHANNELS 3	//1-灰度图像， 3-彩色图像
-
-#define IMAGE_READ_ISCOLOR	1		//读入图像类型，1-原图像， 0 - 强制灰度图像
+#define IMAGE_READ_DEPTH IPL_DEPTH_8U	//读入图像像素的位深度 无符号8bit整型
+#define IMAGE_READ_CHANNELS 3	//图像通道，1-灰度图像， 3-彩色图
+#define IMAGE_READ_ISCOLOR	1	//读入图像类型，1-原图像， 0 - 强制灰度图像
 
 //灰度图像
-#define IMAGE_GRAY_DEPTH IPL_DEPTH_8U
-#define IMAGE_GRAY_CHANNELS 1
+#define IMAGE_GRAY_DEPTH IPL_DEPTH_8U	//位深度
+#define IMAGE_GRAY_CHANNELS 1	//通道
 
 //二进制图像
 #define IMAGE_BINARY_DEPTH IPL_DEPTH_8U
@@ -53,7 +52,7 @@ public:
 	int m_nFacesCount;						//检测人脸的数目
 	bool m_isBinaryMode;					//是否检测二进制模式
 
-	CString m_strErrMessage;					//出错信息
+	CString m_strErrMessage;				//出错信息
 
 public:
 	void Init();							//初始化图像
@@ -63,7 +62,8 @@ public:
 	bool SetReadImage( IplImage* image, int maxWidth=0, int maxHeight=0 );	//设置图片大小并保存到m_pReadImage/m_pGrayImage/m_pBinaryImage
 	bool SetGrayImage( IplImage* image = NULL);		//设置图片大小并保存到m_readImage
 	bool SetBinaryImage(IplImage* image = NULL);		//二值化
-	bool FaceDetect( CString strCascadeName, CString strEyesCascadeName = _T("") );			//人脸检测
+	bool FaceDetect( CString strCascadeName, CString strEyesCascadeName = _T("")
+		);			//人脸检测
 	void Normalization( IplImage* image );		//归一化
 	void RemoveNoise(bool isGrayImage = false);							//去除噪声
 	void EqualizeHist( IplImage *image = NULL);	//灰度图像直方图均衡化
--- trunk/Face/Face/DetectDlg.h
+++ trunk/Face/Face/DetectDlg.h
@@ -3,7 +3,7 @@
 #include "Config.h"
 
 
-#define SHOWIMAGE_WIDTH 350		//显示图片大小
+#define SHOWIMAGE_WIDTH 350		//显示图片控件区域大小
 #define SHOWIMAGE_HEIGHT 330	
 
 // CDetectDlg 对话框
@@ -24,34 +24,33 @@ protected:
 
 	DECLARE_MESSAGE_MAP()
 public:
-	afx_msg void OnBnClickedDtbtnOpendir();
-	afx_msg void OnBnClickedDtbtnOpenimage();
-	afx_msg void OnBnClickedDtbtnSaveimage();
-	afx_msg void OnBnClickedDtbtnDetect();
-	afx_msg void OnBnClickedDtbtnAbout();
+	afx_msg void OnBnClickedDtbtnOpendir();		//打开目录
+	afx_msg void OnBnClickedDtbtnOpenimage();	//打开图像
+	afx_msg void OnBnClickedDtbtnSaveimage();	//保存图像
+	afx_msg void OnBnClickedDtbtnDetect();		//人脸检测
+	afx_msg void OnBnClickedDtbtnAbout();		//关于
 
 
 public:
-	void ResizeImage(IplImage* img);
+	void ResizeImage(IplImage* img);	//显示图像
 	void ShowImage( IplImage* img, UINT ID );	// ID 是Picture Control控件的ID号
-	void ShowImage( IplImage* srcImage);	//显示图像，包括缩小图像
-	void SetTips( CString strTips);
+	void ShowImage( IplImage* srcImage);	//重载，显示图像，包括缩小图像
+	void SetTips( CString strTips);			//提示信息
 
 private:
-	IplImage* m_showImage;
-	CDetect m_objDetect;
-	CConfig m_objConfig;
-	CString m_strCascadeName;
-	CString m_strEyesCascadeName;
-	bool m_blnIsShowGray;
+	CString m_strAppPath;	//程序路径
+	IplImage* m_showImage;	//显示的图像
+	CDetect m_objDetect;	//人脸检测对象
+	CConfig m_objConfig;	//配置文件对象，读取ini文件
+	CString m_strCascadeName;	//人脸级联分类器xml路径
+	CString m_strEyesCascadeName;	//人眼级联分类器xml路径
+	bool m_blnIsShowGray;		//标识位，是否已经显示灰度图像
 
 
 public:
-	virtual BOOL OnInitDialog();
-	afx_msg void OnPaint();
-	// 程序路径
-	CString m_strAppPath;
-	afx_msg void OnBnClickedDtbtn2gray();
-	afx_msg void OnBnClickedDtbtnEqualhist();
-	afx_msg void OnBnClickedDtbtnRemovenoise();
+	virtual BOOL OnInitDialog();	//对话框初始化
+	afx_msg void OnPaint();			//窗口函数
+	afx_msg void OnBnClickedDtbtn2gray();	//灰度图像
+	afx_msg void OnBnClickedDtbtnEqualhist();	//直方图均衡化
+	afx_msg void OnBnClickedDtbtnRemovenoise();	//去除噪声
 };
